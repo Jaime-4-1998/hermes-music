938b41cf9cbac4a666228275d6139418f444129c
jaimeenelabismo <77316145+jaimeenelabismo@users.noreply.github.com>
2021-11-26 12:41:18 -0600

    casi listo para desplegar

--- assets/js/index.js
+++ assets/js/index.js
@@ -141,3 +141,31 @@ duration: 3500,
 origin: 'bottom',
 distance: '90%'
 });  
+
+
+//marcas
+$(function () {
+ 
+var filterList = {
+
+  init: function () {
+  
+      // https://mixitup.kunkalabs.com/
+      $('#gallery').mixItUp({
+          selectors: {
+          target: '.gallery-item',
+          filter: '.filter'	
+      },
+      load: {
+        filter: '.audiodigital' // show all items on page load.
+      }     
+      });								
+  
+  }
+
+};
+
+// Filter ALL the things
+filterList.init();
+
+});
