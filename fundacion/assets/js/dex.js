2d0830de544d1dcee0ba30585a30e8d987ce6c3b
Jaime-4-1998 <71422106+Jaime-4-1998@users.noreply.github.com>
2022-02-01 06:20:51 +0000

    Deploying to gh-pages from @ Jaime-4-1998/hermes-music@048325f0a630ffcee880bba819be5210d6dc0948 🚀

---
+++ fundacion/assets/js/dex.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".mySwiper2", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+//menu
+(function($) { // Begin jQuery
+    $(function() { // DOM ready
+      // If a link has a dropdown, add sub menu toggle.
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      // Clicking away from dropdown will remove the dropdown class
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+        
+      });
+      // Toggle open and close nav styles on click
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+     
+    }); // end DOM ready
+  })(jQuery); // end jQuery
